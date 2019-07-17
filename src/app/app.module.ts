4b2dad2d267334f9a4460511d0c4752eee6d336b
hazalbulut <hazal@evtiko.com>
2019-07-17 17:04:54 +0300

    modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './views/home/home.module';
+import { ExampleModule } from './views/example/example.module';
+import { ProductDetailModule } from './views/product-detail/product-detail.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HomeModule,
+        ExampleModule,
+        ProductDetailModule,
+        ReactiveFormsModule
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
